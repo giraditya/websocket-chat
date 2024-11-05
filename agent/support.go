2cb3514ef01fe2d710c553b0990150b5419df445
giri <giri.aditya@ottodigital.id>
2024-11-06 00:14:44 +0700

    remove unused code

--- agent/support.go
+++ agent/support.go
@@ -1,27 +1,9 @@
 package agent
 
-import (
-	"fmt"
-	"websocket-chat/models"
-)
+type SupportAgent struct{}
 
-type SupportAgent struct {
-	Username    string
-	ChatHistory []models.Message
-}
-
-type SupportAgentInterface interface {
-	SendMessage(msg models.Message, wsCon models.WebsocketConnection) error
-}
+type SupportAgentInterface interface{}
 
 func NewSupportAgent() SupportAgentInterface {
 	return &SupportAgent{}
-}
-
-func (a *SupportAgent) SendMessage(msg models.Message, wsCon models.WebsocketConnection) error {
-	err := wsCon.Conn.WriteJSON(msg)
-	if err != nil {
-		return fmt.Errorf("error send message to user: %v", err)
-	}
-	return nil
 }
